0fe7fb6b1942131a2cb2683155c56bf77ac70ea8
edvinsternvik <e.sternvik@gmail.com>
2024-08-09 14:17:20 +0200

    Update diffview.nvim config

--- base/.config/nvim/lua/plugins/neogit.lua
+++ base/.config/nvim/lua/plugins/neogit.lua
@@ -1,29 +1,19 @@
 return {
-    {
-        "NeogitOrg/neogit",
-        cmd = "Neogit",
-        dependencies = {
-            "plenary.nvim",
-            "diffview.nvim",
-            "telescope.nvim",
-        },
-        opts = {
-            graph_style = "unicode",
-            integrations = {
-                telescope = true,
-                diffview = true,
-            },
-        },
-        keys = {
-            { "<leader>gg", ":Neogit<CR>" },
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+        "plenary.nvim",
+        "diffview.nvim",
+        "telescope.nvim",
+    },
+    opts = {
+        graph_style = "unicode",
+        integrations = {
+            telescope = true,
+            diffview = true,
         },
     },
-    {
-        "sindrets/diffview.nvim",
-        lazy = true,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-    }
-
+    keys = {
+        { "<leader>gg", ":Neogit<CR>" },
+    },
 }
