b3e1f7a12ee218beda0b440b35e14089decdaaf2
edvinsternvik <e.sternvik@gmail.com>
2025-03-28 12:37:29 +0100

    Neovim: Add TypstPin user command

--- base/.config/nvim/lua/plugins/typst.lua
+++ base/.config/nvim/lua/plugins/typst.lua
@@ -2,4 +2,41 @@ return {
     'kaarmu/typst.vim',
     ft = 'typst',
     lazy=false,
+    init = function()
+        vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+            pattern = {"*.typ"},
+            callback = function()
+                vim.opt_local.tabstop = 4
+            end,
+        })
+
+        vim.api.nvim_create_user_command("TypstPin", function()
+          local tinymist_id = nil
+          for _, client in pairs(vim.lsp.get_active_clients()) do
+            if client.name == "tinymist" then
+              tinymist_id = client.id
+              break
+            end
+          end
+
+          if not tinymist_id then
+            vim.notify("tinymist not running!", vim.log.levels.ERROR)
+            return
+          end
+
+          local client = vim.lsp.get_client_by_id(tinymist_id)
+          if client then
+            client.request("workspace/executeCommand", {
+              command = "tinymist.pinMain",
+              arguments = { vim.api.nvim_buf_get_name(0) },
+            }, function(err)
+              if err then
+                vim.notify("error pinning: " .. err, vim.log.levels.ERROR)
+              else
+                vim.notify("succesfully pinned", vim.log.levels.INFO)
+              end
+            end, 0)
+          end
+        end, {})
+    end
 }
