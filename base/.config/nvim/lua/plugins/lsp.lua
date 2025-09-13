3bd6840fd3c371467891d94a6342453147b8a2b7
edvinsternvik <e.sternvik@gmail.com>
2025-09-13 12:47:15 +0200

    Add doc toggle for neovim cmp

--- base/.config/nvim/lua/plugins/lsp.lua
+++ base/.config/nvim/lua/plugins/lsp.lua
@@ -50,10 +50,10 @@ end
 
 cmp_setup = function(_, opts)
     local cmp = require("cmp")
+
     cmp.setup({
         snippet = {
             expand = function(args)
-                -- require('luasnip').lsp_expand(args.body)
                 vim.snippet.expand(args.body)
             end,
         },
@@ -62,12 +62,22 @@ cmp_setup = function(_, opts)
             completion = cmp.config.window.bordered(),
             documentation = cmp.config.window.bordered(),
         },
+        view = {                                                        
+            docs = { auto_open = false },
+        },                                                               
         mapping = cmp.mapping.preset.insert({
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.abort(),
             ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            ['<C-g>'] = function()
+                if cmp.visible_docs() then
+                  cmp.close_docs()
+                else
+                  cmp.open_docs()
+                end
+              end,
         }),
         sources = cmp.config.sources(
             {
