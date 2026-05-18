d4703eadbc472f6db0ed1aae35c7c933f14366bb
edvinsternvik <e.sternvik@gmail.com>
2026-05-18 21:21:38 +0200

    Neovim: Disable cornelis

--- base/.config/nvim/lua/plugins/agda.lua
+++ base/.config/nvim/lua/plugins/agda.lua
@@ -1,32 +1,32 @@
 return {
-    "isovector/cornelis",
-    ft = { "agda" },
-    build = "stack build",
-    dependencies = {
-        "kana/vim-textobj-user",
-        "neovimhaskell/nvim-hs.vim",
-    },
-    init = function()
-        vim.cmd [[
-            au BufRead,BufNewFile *.agda call AgdaFiletype()
-            au QuitPre *.agda :CornelisCloseInfoWindows
-            function! AgdaFiletype()
-                nnoremap <buffer> <leader>cl :CornelisLoad<CR>:sleep 100ms<CR><Enter>:CornelisQuestionToMeta<CR>
-                nnoremap <buffer> <leader>c? :CornelisGoals<CR>
-                nnoremap <buffer> <leader>cg :CornelisGive<CR>
-                nnoremap <buffer> <leader>cr :CornelisRefine<CR>
-                nnoremap <buffer> <leader>cc :CornelisMakeCase<CR>
-                nnoremap <buffer> <leader>c, :CornelisTypeContext<CR>
-                nnoremap <buffer> <leader>c. :CornelisTypeContextInfer<CR>
-                nnoremap <buffer> <leader>cs :CornelisSolve<CR>
-                nnoremap <buffer> <leader>ca :CornelisAuto<CR>
-                nnoremap <buffer> <leader>cb :CornelisPrevGoal<CR>
-                nnoremap <buffer> <leader>cf :CornelisNextGoal<CR>
-                nnoremap <buffer> <leader>cn :CornelisNormalize<CR>
-            endfunction
-            let g:cornelis_no_agda_input = 1
-        ]]
+    -- "isovector/cornelis",
+    -- ft = { "agda" },
+    -- build = "stack build",
+    -- dependencies = {
+    --     "kana/vim-textobj-user",
+    --     "neovimhaskell/nvim-hs.vim",
+    -- },
+    -- init = function()
+    --     vim.cmd [[
+    --         au BufRead,BufNewFile *.agda call AgdaFiletype()
+    --         au QuitPre *.agda :CornelisCloseInfoWindows
+    --         function! AgdaFiletype()
+    --             nnoremap <buffer> <leader>cl :CornelisLoad<CR>:sleep 100ms<CR><Enter>:CornelisQuestionToMeta<CR>
+    --             nnoremap <buffer> <leader>c? :CornelisGoals<CR>
+    --             nnoremap <buffer> <leader>cg :CornelisGive<CR>
+    --             nnoremap <buffer> <leader>cr :CornelisRefine<CR>
+    --             nnoremap <buffer> <leader>cc :CornelisMakeCase<CR>
+    --             nnoremap <buffer> <leader>c, :CornelisTypeContext<CR>
+    --             nnoremap <buffer> <leader>c. :CornelisTypeContextInfer<CR>
+    --             nnoremap <buffer> <leader>cs :CornelisSolve<CR>
+    --             nnoremap <buffer> <leader>ca :CornelisAuto<CR>
+    --             nnoremap <buffer> <leader>cb :CornelisPrevGoal<CR>
+    --             nnoremap <buffer> <leader>cf :CornelisNextGoal<CR>
+    --             nnoremap <buffer> <leader>cn :CornelisNormalize<CR>
+    --         endfunction
+    --         let g:cornelis_no_agda_input = 1
+    --     ]]
 
-        vim.g.cornelis_split_location = "bottom"
-    end,
+    --     vim.g.cornelis_split_location = "bottom"
+    -- end,
 }
--- base/.config/nvim/lua/plugins/telescope.lua
+++ base/.config/nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ end
 
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    tag = 'v0.2.2',
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
