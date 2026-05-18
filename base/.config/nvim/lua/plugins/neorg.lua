7172d62d2d2fb0306cf1a2ff18451d3cd8b753f4
edvinsternvik <e.sternvik@gmail.com>
2026-05-18 21:36:30 +0200

    Neovim: Disable neorg by default

--- base/.config/nvim/lua/plugins/neorg.lua
+++ base/.config/nvim/lua/plugins/neorg.lua
@@ -1,37 +1,37 @@
-neorg_config = function()
-    require("neorg").setup {
-        load = {
-            ["core.defaults"] = {},
-            ["core.concealer"] = {},
-            ["core.dirman"] = {
-                config = {
-                    workspaces = {
-                        notes = "~/Documents/notes",
-                    },
-                    default_workspace = "notes",
-                },
-            },
-            ["core.summary"] = {}
-        },
-    }
+-- neorg_config = function()
+--     require("neorg").setup {
+--         load = {
+--             ["core.defaults"] = {},
+--             ["core.concealer"] = {},
+--             ["core.dirman"] = {
+--                 config = {
+--                     workspaces = {
+--                         notes = "~/Documents/notes",
+--                     },
+--                     default_workspace = "notes",
+--                 },
+--             },
+--             ["core.summary"] = {}
+--         },
+--     }
 
-    vim.wo.foldlevel = 99
-    vim.wo.conceallevel = 2
-end
+--     vim.wo.foldlevel = 99
+--     vim.wo.conceallevel = 2
+-- end
 
 return {
-    "nvim-neorg/neorg",
-    tag = "v7.0.0",
-    build = ":Neorg sync-parsers",
-    ft = "norg",
-    cmd = "Neorg",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = neorg_config,
-    keys = {
-        { "<leader>ni", ":Neorg index<cr>" },
-        { "<leader>nq", ":Neorg return<cr>" },
-    },
+    -- "nvim-neorg/neorg",
+    -- tag = "v9.6.4",
+    -- ft = "norg",
+    -- cmd = "Neorg",
+    -- dependencies = {
+    --     "nvim-lua/plenary.nvim",
+    --     "nvim-treesitter/nvim-treesitter",
+    -- },
+    -- config = neorg_config,
+    -- keys = {
+    --     { "<leader>ni", ":Neorg index<cr>" },
+    --     { "<leader>nq", ":Neorg return<cr>" },
+    -- },
+    -- lazy = true,
 }
